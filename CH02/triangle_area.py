1aa39ad24aff59b81ee7f4f0931dc735ddd39de0
tcaine2 <caine1013@gmail.com>
2024-09-18 18:53:45 +0000

    triangle_area

---
+++ CH02/triangle_area.py
@@ -0,0 +1,91 @@
+# FILE NAME - triangle_area.py
+
+# NAME: Thomas Caine
+# DATE: 9/18/24
+# BRIEF DESCRIPTION:  This program will create a trangle with the * symbol using formulas
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+def main():
+    find_area()
+
+
+
+def find_area():
+
+    ########## ENTER YER CODE BELOW THIS LINE ##########
+    
+    triangle_height = input("Enter the height: ")
+    triangle_base = input("Enter the base: ")
+
+    print('The area of the triangle is')
+    triangle_area = (0.5*triangle_height*triangle_base)
+    print(triangle_area)
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    ########### END YER CODE ABOVE THIS LINE ###########
+
+main()
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+Enter the height: 1
+Enter the base: 1
+
+The area of the triangle is 0.5
+
+'''
+
+
+
+'''
+
+Enter the height: 8
+Enter the base: 4
+
+The area of the triangle is 16.0
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What is the flow of the program? Which line of code kicks off the process?
+
+
+
+
+
+2. What was the hardest part of this lab?
+
+
+
+
+
+'''
