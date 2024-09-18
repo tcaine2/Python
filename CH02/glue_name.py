b5832d6396d06dc32cc3e38e89454e8773321560
tcaine2 <caine1013@gmail.com>
2024-09-18 18:25:01 +0000

    glue_name

---
+++ CH02/glue_name.py
@@ -0,0 +1,94 @@
+# FILE NAME - glue_name.py
+
+# NAME: Thomas Caine
+# DATE: 09/18/24
+# BRIEF DESCRIPTION: This is a code that will output text by repeating names
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. You can see the Sample Output below, too
+
+
+
+def main():
+    glue_name()
+
+
+
+def glue_name():
+
+    ########## ENTER YER CODE BELOW THIS LINE ##########
+
+    first_name = input("Enter first name: ")
+    last_name = input("Enter last name: ")
+    
+    print(f'Hi, {first_name} {last_name}')
+
+    ########### END YER CODE ABOVE THIS LINE ###########
+
+main()
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+Dave
+Ghidiu
+Hi, Dave Ghidiu
+
+'''
+
+
+
+'''
+
+bart
+simpson
+Hi, bart simpson
+
+'''
+
+
+
+'''
+
+Paige
+LaBarr
+Hi, Paige LaBarr
+
+'''
+
+
+
+'''
+
+Lisa
+Simpson, esquire
+Hi, Lisa Simpson, esquire
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What is a lesson you learned during this lab? You do not have to restrict
+   your response to Python. It might be a lesson in time management, a lesson
+   using CodeGrade, or something else.
+
+
+
+
+
+'''
