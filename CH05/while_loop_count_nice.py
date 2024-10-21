0a93db5ad4fa8ed63f36370df93e7558a613d1d6
tcaine2 <caine1013@gmail.com>
2024-10-21 15:19:24 +0000

    while_loop_count_nice

---
+++ CH05/while_loop_count_nice.py
@@ -0,0 +1,81 @@
+# FILE NAME - while_loop_count_nice.py
+
+# NAME: Thomas Caine
+# DATE: 10/21/24
+# BRIEF DESCRIPTION: This lab will function the same as the while_loop_count lab but 
+# instead of outputting each number on a separate line we will output them all on one 
+# line with a comma and a space between them. 
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+# You must create a main() function that calls your function 
+# that runs the loop.
+
+# You also have to make a call to main() in the program.
+
+def main(): 
+    while_loop_count()
+
+def while_loop_count():
+    number = 0
+    while number < 9:
+        print(number, ', ', end='')
+        number += 1
+    print(number)
+
+main()
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+'''
+
+
+
+########################################
+#            ATTESTATION
+########################################
+
+'''
+
+Please gauge your utilization of AI on the following spectrum. Place an "X" in front
+of the appropriate response. Only choose one of the following:
+
+[ ] I did not use AI at all for this lab.
+[ ] I wrote the initial draft of the software but had AI help me make it better.
+[ ] I fed the lab description to AI and had it generate a response but I modified it.
+[ ] AI created the entire program for me.
+
+
+
+It is critical in this class that you understand the concepts as we explore them because
+those concepts are required understanding for entry level programming. Reliance on resources
+like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
+may impede your understanding. Please rate how well you understand the concepts in this lab: 
+
+[ ] I understand very little about this lab.
+[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
+[ ] I pretty much get it.
+[ ] I'm solid. Totally got it.
+
+'''
