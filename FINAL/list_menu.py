4c787fd0ca433712e758882113fe00b77610bdd9
tcaine2 <caine1013@gmail.com>
2024-12-16 18:33:27 +0000

    list_menu

---
+++ FINAL/list_menu.py
@@ -0,0 +1,18 @@
+# Name: Thomas Caine
+# Date: 12/16/24
+# Program description: This program will display all the sticky notes from the file "StickyNotez"
+
+def list_menu():
+    print()
+    # sticky_list = ["1. Grocery List", 
+    #                "2. Movies to watch", 
+    #                "3. Palindromes", 
+    #                "4. QUIT"]
+    
+    print('1. Grocery List')
+    print('2. Movies to watch')
+    print('3. Palindromes')
+    print('4. QUIT PROGRAM')
+
+    # print('list_menu() was called')
+list_menu()
--- FINAL/stickynotez_banner.py
+++ FINAL/stickynotez_banner.py
@@ -2,8 +2,10 @@
 # Date: 12/16/24
 # Program description: This program will display a banner for the software package called "StickyNotez"
 
-# def stickynotez_banner():
-print('===================')
-print('    STICKYNOTEZ    ')
-print('===================')
-# print('stickynotez_banner() was called')
+def stickynotez_banner():
+    print('===================')
+    print('    STICKYNOTEZ    ')
+    print('===================')
+
+    # print('stickynotez_banner() was called')
+stickynotez_banner()
