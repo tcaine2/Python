dab74d4432e5a2357eb3b0ae6df45aa16f7d707d
tcaine2 <caine1013@gmail.com>
2024-12-16 18:14:38 +0000

    stickynotez_banner

---
+++ FINAL/stickynotez_banner.py
@@ -0,0 +1,9 @@
+# Name: Thomas Caine
+# Date: 12/16/24
+# Program description: This program will display a banner for the software package called "StickyNotez"
+
+# def stickynotez_banner():
+print('===================')
+print('    STICKYNOTEZ    ')
+print('===================')
+# print('stickynotez_banner() was called')
